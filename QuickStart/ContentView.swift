088f1ec788935e10871daafab9760fdf70cfecb1
Yuning30 <1987150762@qq.com>
2024-05-30 23:36:58 -0400

    Initial Commit

---
+++ QuickStart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickStart
+//
+//  Created by Yuning Wang on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickStart/QuickStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickStartApp.swift
+//  QuickStart
+//
+//  Created by Yuning Wang on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
