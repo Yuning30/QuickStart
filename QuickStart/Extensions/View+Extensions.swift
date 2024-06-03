f961657de01108459c7402e37c412e5c3b791f16
Yuning30 <1987150762@qq.com>
2024-06-02 21:10:00 -0400

    correct calculate the angle for different directions

---
+++ QuickStart/Extensions/View+Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  View+Extensions.swift
+//  Loop
+//
+//  Created by Kai Azim on 2023-06-14.
+//
+
+import SwiftUI
+
+extension View {
+    // Make it easier to receive notifications SwiftUI views
+    func onReceive(
+        _ name: Notification.Name,
+        center: NotificationCenter = .default,
+        object: AnyObject? = nil,
+        perform action: @escaping (Notification) -> Void
+    ) -> some View {
+        self.onReceive(
+            center.publisher(for: name, object: object),
+            perform: action
+        )
+    }
+
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
--- QuickStart/RadialMenu/RadialMenuView.swift
+++ QuickStart/RadialMenu/RadialMenuView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 import Combine
 import Defaults
+import UserNotifications
 
 struct RadialMenuView: View {
     let radialMenuSize: CGFloat = 100
     var radialMenuCornerRadius: CGFloat = 50
     var radialMenuThickness: CGFloat = 22
-    
+    var previewMode = false
     @State var angle: Double = .zero
+    @State var currentAction: ActionDirection = .noAction
+    @State var previousAction: ActionDirection?
 
     var body: some View {
         VStack {
@@ -33,52 +36,29 @@ struct RadialMenuView: View {
                                     .overlay {
                                         ZStack {
                                             ZStack {
-                                                if radialMenuCornerRadius >= radialMenuSize / 2 - 2 {
                                                     DirectionSelectorCircleSegment(
                                                         angle: self.angle,
                                                         radialMenuSize: self.radialMenuSize
                                                     )
-                                                } else {
-                                                    DirectionSelectorSquareSegment(
-                                                        angle: self.angle,
-                                                        radialMenuCornerRadius: self.radialMenuCornerRadius,
-                                                        radialMenuThickness: self.radialMenuThickness
-                                                    )
-                                                }
                                             }
                                             .compositingGroup()
                                         }
                                     }
                             }
-
-                        if radialMenuCornerRadius >= radialMenuSize / 2 - 2 {
+                        
                             Circle()
                                 .stroke(.quinary, lineWidth: 2)
 
                             Circle()
                                 .stroke(.quinary, lineWidth: 2)
                                 .padding(self.radialMenuThickness)
-                        } else {
-                            RoundedRectangle(cornerRadius: radialMenuCornerRadius, style: .continuous)
-                                .stroke(.quinary, lineWidth: 2)
-
-                            RoundedRectangle(
-                                cornerRadius: radialMenuCornerRadius - self.radialMenuThickness,
-                                style: .continuous
-                            )
-                            .stroke(.quinary, lineWidth: 2)
-                            .padding(self.radialMenuThickness)
-                        }
                     }
                     // Mask the whole ZStack with the shape the user defines
                     .mask {
-                        if radialMenuCornerRadius >= radialMenuSize / 2 - 2 {
+
                             Circle()
                                 .strokeBorder(.black, lineWidth: radialMenuThickness)
-                        } else {
-                            RoundedRectangle(cornerRadius: radialMenuCornerRadius, style: .continuous)
-                                .strokeBorder(.black, lineWidth: radialMenuThickness)
-                        }
+                        
                     }
                 }
                 .frame(width: radialMenuSize, height: radialMenuSize)
@@ -88,12 +68,62 @@ struct RadialMenuView: View {
             Spacer()
         }
         .shadow(radius: 10)
-
-        // Animate window
+        .scaleEffect(currentAction == .noAction ? 0.85 : 1)
+        .animation(.easeOut(duration: 0.2), value: currentAction)
+        .onAppear {
+            if previewMode {
+                currentAction = currentAction
+            }
+        }
         
+        // Animate window
+        .onReceive(Notification.Name.updateUIDirection) { 
+            obj in
+            if !self.previewMode, let action = obj.userInfo?["action"] as? ActionDirection {
+                self.previousAction = self.currentAction
+                self.currentAction = action
+
+                print("New radial menu window action received: \(action)")
+            }
+        }
+        .onChange(of: self.currentAction) { _ in
+            switch self.currentAction {
+            case .noAction:
+                self.angle = 0
+            case .right:
+                self.angle = 0
+            case .bottomRight:
+                self.angle = 45
+            case .bottom:
+                self.angle = 90
+            case .bottomLeft:
+                self.angle = 135
+            case .left:
+                self.angle = 180
+            case .topLeft:
+                self.angle = 225
+            case .top:
+                self.angle = 270
+            case .topRight:
+                self.angle = 315
+            }
+//            if let target = self.currentAction.radialMenuAngle(window: window) {
+//                let closestAngle: Angle = .degrees(self.angle).angleDifference(to: target)
+//
+//                let previousActionHadAngle = self.previousAction?.direction.hasRadialMenuAngle ?? false
+//                let animate: Bool = abs(closestAngle.degrees) < 179 && previousActionHadAngle
+//
+//                let defaultAnimation = AnimationConfiguration.fast.radialMenuAngle
+//                let noAnimation = Animation.linear(duration: 0)
+//
+//                withAnimation(animate ? defaultAnimation : noAnimation) {
+//                    self.angle += closestAngle.degrees
+//                }
+//            }
+        }
     }
 }
 
-#Preview {
-    RadialMenuView()
-}
+//#Preview {
+//    RadialMenuView()
+//}
